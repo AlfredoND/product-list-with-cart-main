79096a1a69ba583f146712188a2fd52b3f16f368
Ancode <alfayomega5027@gmail.com>
2025-01-05 22:36:34 -0300

    add package.json file, reset.css and initial styles; update .gitignore to include node_modules

--- main.js
+++ main.js
@@ -1,46 +1,44 @@
-const render = document.getElementById('render');
+const render = document.getElementById('render')
 
 fetch('data.json')
   .then(res => res.json())
   .then(data => renderHtml(data))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err))
 
-function renderHtml(data) {
-
-  //generate a counter for each element in the data array
-  let counters = Array(data.length).fill(0).map(() => ({ counter: 0 }));
+function renderHtml (data) {
+  // generate a counter for each element in the data array
+  const counters = Array(data.length).fill(0).map(() => ({ counter: 0 }))
 
   // image renderer depending on width of the screen
-  function rendererImg(index) {
+  function rendererImg (index) {
     //! añadir validacion de 'data' y  'data[index]'
     //! hacer que el cambio de url no dependa de refrescar la pagina
-    //TODO refactorizar la validación de tamaña de pantalla
+    // TODO refactorizar la validación de tamaña de pantalla
     //* añadir a la validacion de tamaño de pantalla la imagen para tablet
-    //TODO aplicar destructuracion a el 'elemnent'
+    // TODO aplicar destructuracion a el 'elemnent'
 
-    let element = data[index]
+    const element = data[index]
     if (window.innerWidth >= 1440) {
-      console.log(element.image.desktop || ' ')
+      console.log(element.image.desktop || 'imagen no encontrada')
       return element.image.desktop || ' '
     } else if (window.innerWidth <= 375) {
-      console.log(element.image.mobile || ' ')
+      console.log(element.image.mobile || 'imagen no encontrada')
       return element.image.mobile || ' '
     } else {
       console.error('no se pudo renderizar la imagen')
     }
-
-
   }
 
-  render.innerHTML = ''; // clean before rendering
+  render.innerHTML = '' // clean before rendering
 
+  data.forEach(({ name, category, price }, index) => {
+    // create card, asign class and id
+    const card = document.createElement('article')
+    card.className = 'card'
+    card.id = index
 
-  data.forEach((element, index) => {
-    //create card, asign class and id
-    const card = document.createElement('article');
-    card.className = 'card';
-    card.id = index;
-    //TODO añadir precisión de doble cero al precio
+    // TODO añadir precisión de doble cero al precio
+    // TODO hacer que se muestre buttons_add_cart cunado 'counter' es 0 y si es distinto de 0 que muestre card_buttons
     // create template
     card.innerHTML = `
       <img src=${rendererImg(index)} alt="img" />
@@ -56,32 +54,26 @@ function renderHtml(data) {
       </div>
 
       <div class="card_info">
-        <span>${element.category}</span>
-        <h5>${element.name}</h5>
-        <span>$${element.price}</span>
+        <span>${category}</span>
+        <h5>${name}</h5>
+        <span>$${price}</span>
       </div>
-    `;
-    //render card in 'render'
-    render.appendChild(card);
-  });
-  //! por documentar 
-  //TODO implementar localStorage para guardar los datos de los contadores
-  
+    `
+    // render card in 'render'
+    render.appendChild(card)
+  })
+  //! por documentar
+  // TODO implementar localStorage para guardar los datos de los contadores
+
   render.addEventListener('click', (e) => {
     if (e.target.closest('.add')) {
-      const index = e.target.closest('.add').dataset.index;
-      counters[index].counter++;
-      document.querySelector(`.amount[data-index="${index}"]`).innerText = counters[index].counter;
+      const index = e.target.closest('.add').dataset.index
+      counters[index].counter++
+      document.querySelector(`.amount[data-index="${index}"]`).innerText = counters[index].counter
     } else if (e.target.closest('.subtract')) {
-      const index = e.target.closest('.subtract').dataset.index;
-      counters[index].counter = Math.max(0, counters[index].counter - 1);
-      document.querySelector(`.amount[data-index="${index}"]`).innerText = counters[index].counter;
+      const index = e.target.closest('.subtract').dataset.index
+      counters[index].counter = Math.max(0, counters[index].counter - 1)
+      document.querySelector(`.amount[data-index="${index}"]`).innerText = counters[index].counter
     }
-  });
+  })
 }
-
-
-// * add to button_add 
-{/* <button class="button_add_cart">
-<img src="assets/images/icon-add-to-cart.svg" alt="" />
-</button> */}
